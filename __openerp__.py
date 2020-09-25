788217d9c40631a1234953bee2d6e46e89b3059d
leaverock <minor3994@rambler.ru>
2020-09-25 17:24:36 +0300

    Исправления

--- __openerp__.py
+++ __openerp__.py
@@ -8,6 +8,7 @@
     'version': '0.1',
     'depends': ['eco'],
     'data': [
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/menu.xml',
         'data/eco_pret_isk_problems_view_content.xml',
